22e755c4637d6be9864a60ca3bde518af986c59a
Leandro-Suero <ing.leandrosuero@gmail.com>
2020-03-26 18:21:07 -0300

    Cleaning up

--- src/components/ExerciceList.js
+++ src/components/ExerciceList.js
@@ -2,17 +2,7 @@ import React from "react";
 import Card from "../components/Card";
 
 const ExerciceList = ({ data }) => {
-    console.log(data);
-    return data.map(exercice => (
-        <Card
-            key={exercice.id}
-            title={exercice.title}
-            description={exercice.description}
-            img={exercice.img}
-            leftColor={exercice.leftColor}
-            rightColor={exercice.rightColor}
-        />
-    ));
+    return data.map(exercice => <Card key={exercice.id} {...exercice} />);
 };
 
 export default ExerciceList;
